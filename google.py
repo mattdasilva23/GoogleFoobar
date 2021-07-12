e525ed690351428d1e90ce856dbd8835fc19dffe
mattdasilva23 <mattdasilva23@hotmail.co.uk>
2021-07-12 01:53:54 +0100

    gitignore

--- google.py
+++
@@ -1,7 +0,0 @@
-
-# complaints so far:
-
-# - why are you running python 2.7 (fixed with shebang anyway)
-# - why dont you tell us what the errors are in some of the test runs, e.g. we spent hours on one of the problems before and realised it was just a simple syntax error - spent like 3 hours finding it
-# - LVL4 Q2 read bottom part (stuff about hitting corners at a non 45 degree angle)
-# - SYMPY DOES NOT WORK
